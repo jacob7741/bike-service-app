14315d5c480158de3cffbacfda4d247b4f86b1fc
Jacob Sunny <jacobsunny@macbook.local>
2024-10-25 08:42:16 +0200

    update

---
+++ src/main/java/bike/service/app/controller/MainController.java
@@ -0,0 +1,17 @@
+package bike.service.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+//    z response zwraca napis z metody a bez tego nazwe
+//    pliku ktory znajduje sie w templates
+   // @ResponseBody
+    public String home() {
+        return "mainSite";
+    }
+}
