5fa7b7436a72b5eba4e2bc9608e34d7332492c4b
Jacob Sunny <sjakub7741@gmail.com>
2024-10-26 17:08:10 +0200

    dataBase 1.02

--- src/main/java/bike/service/app/data/DataBaseReader.java
+++ src/main/java/bike/service/app/data/DataBaseReader.java
@@ -4,10 +4,13 @@ import java.sql.*;
 
 public class DataBaseReader {
     public static void main(String[] args) {
+
+        //płączenie z bazą danych
         try {
             Connection connection = DriverManager.getConnection("jdbc:h2:/Users/jacobsunny/BServiceProject/src/main/resources/bikeServices",
                     "sa","password" );
             Statement statement = connection.createStatement();
+            //tutaj pobieram info z BikeService o serwisach i ich id
             ResultSet resultSet = statement.executeQuery("SELECT * FROM BikeService");
 
             while (resultSet.next()) {
