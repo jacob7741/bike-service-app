f313aafecf3dab400384468d7ff013d48858b897
Jacob Sunny <sjakub7741@gmail.com>
2024-11-01 22:27:43 +0100

    update011124

--- src/main/java/bike/service/app/config/ServicesConfig.java
+++ src/main/java/bike/service/app/config/ServicesConfig.java
@@ -5,8 +5,6 @@ import bike.service.app.model.repository.ServicesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
--- src/main/java/bike/service/app/controller/ServiceController.java
+++ src/main/java/bike/service/app/controller/ServiceController.java
@@ -1,9 +1,21 @@
 package bike.service.app.controller;
 
+import bike.service.app.model.repository.ServicesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ServiceController {
 
+    @Autowired
+    ServicesRepository servicesRepository;
 
+    @PostMapping("/services/submit")
+
+    public String createService() {
+
+        return "mainSite";
+    }
 }
