57e3a356a36b721c2b83ac110508b148d409c045
Jacob Sunny <sjakub7741@gmail.com>
2024-12-05 15:23:44 +0100

    loginUpdate

--- src/main/java/bike/service/app/controller/LoginController.java
+++ src/main/java/bike/service/app/controller/LoginController.java
@@ -1,13 +1,24 @@
 package bike.service.app.controller;
 
+import bike.service.app.model.Mechanic;
+import bike.service.app.service.MechanicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class LoginController {
+    @Autowired
+    private MechanicService mechanicService;
 
     @GetMapping("/mainSite")
-    public String mainSite() {
+    public String mainSite(Model model) {
+        System.out.println("application start");
+        List<Mechanic> mechanics = mechanicService.getAllMechanics();
+        model.addAttribute("mechanics", mechanics);
         return "mainSite";
     }
 
