0848f645274fe4537efd5ef7f7714d6c7f77f0bc
Jakub Świątkowski <sjakub7741@gmail.com>
2024-12-06 15:19:59 +0100

    update061224

--- src/main/java/bike/service/app/conf/SecurityConfig.java
+++ src/main/java/bike/service/app/conf/SecurityConfig.java
@@ -30,19 +30,6 @@ public class SecurityConfig {
     private DataSource dataSource;
 
     @Bean
-    public static JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
-        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
-        manager.setUsersByUsernameQuery("SELECT user_name AS username, password, true AS enabled FROM Mechanic WHERE user_name = ?");
-        manager.setAuthoritiesByUsernameQuery("SELECT user_name AS username, 'ROLE_MECHANIC' AS authority FROM Mechanic WHERE user_name = ?");
-        return manager;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
@@ -63,35 +50,18 @@ public class SecurityConfig {
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
         return http.build();
     }
-    //
-//    @Autowired
-//    void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .userDetailsService(userDetailsService)
-//                .passwordEncoder(new BCryptPasswordEncoder());
-//    }
-//    @Bean
-//    public DataSource dataSource() {
-//        return new EmbeddedDatabaseBuilder()
-//                .setType(EmbeddedDatabaseType.H2)
-//                .addScripts("src/main/resources/schema.sql")
-//                .build();
-//    }
-//      wersja testowa
-//    @Bean
-//    public UserDetailsService mechanicDetail() {
-//
-//
-//        List<Mechanic> mechanicList = mechanicService.getAllMechanics();
-//
-//        Mechanic mechanic = mechanicList.get(1);
-//
-//        UserDetails userDetails = User.builder()
-//                .username(mechanic.getUser_name())
-//                .password("{noop}" + mechanic.getPassword())
-//                .roles("MECHANIC")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(userDetails);
-//    }
+//    connection with db without security just for the test !!!
+    @Bean
+    public static JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+        manager.setUsersByUsernameQuery("SELECT user_name AS username, password, true AS enabled FROM Mechanic WHERE user_name = ?");
+        manager.setAuthoritiesByUsernameQuery("SELECT user_name AS username, 'ROLE_MECHANIC' AS authority FROM Mechanic WHERE user_name = ?");
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
