86da50367a8fb13129769826c5ebebf1f2807344
Jakub Świątkowski <sjakub7741@gmail.com>
2025-03-02 22:18:48 +0100

    frontUpdate

--- target/classes/templates/main.js
+++ target/classes/templates/main.js
@@ -6,3 +6,11 @@ el.innerHTML = today.toDateString();
 var cr = document.getElementById('footer');
 cr.innerHTML = today.getFullYear() + ' Copyright©';
 
+
+function showContent(id) {
+    var contents = document.getElementsByClassName('content');
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].style.display = 'none';
+    }
+    document.getElementById(id).style.display = 'block';
+};
