05f3bf3c5fc4843ac0923e3602cd2c9f4a0cc447
Jakub Świątkowski <sjakub7741@gmail.com>
2025-03-07 14:58:10 +0100

    update

---
+++ src/main/java/bike/service/app/controller/NewsRSSController.java
@@ -0,0 +1,9 @@
+package bike.service.app.controller;
+
+import java.net.URL;
+
+public class NewsRSSController {
+
+    String feedUrl = "https://portal.bikeworld.pl/rss/archiwum/32/nowosci_sprzetowe";
+    
+}
--- src/main/java/bike/service/app/service/userroles/MechanicService.java
+++ src/main/java/bike/service/app/service/userroles/MechanicService.java
@@ -6,29 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import bike.service.app.model.Order;
-import bike.service.app.model.repository.BikeRepository;
-import bike.service.app.model.repository.ClientRepository;
 import bike.service.app.model.repository.OrderRepository;
-import bike.service.app.model.repository.ServicesRepository;
-import bike.service.app.service.UsersService;
+
 
 @Service
 public class MechanicService {
 
     @Autowired
     private OrderRepository orderRepository;
-    @Autowired
-    private ClientRepository clientRepository;
-    @Autowired
-    private BikeRepository bikeRepository;
-    @Autowired
-    private ServicesRepository servicesRepository;
-    @Autowired
-    private UsersService userService;
 
     public void doneStatusById(int id) {
         Optional<Order> optional = orderRepository.findById(id);
-        if(optional.isPresent()) {
+        if (optional.isPresent()) {
             Order newOrder = optional.get();
             newOrder.setStatus(Order.Status.DONE);
 
