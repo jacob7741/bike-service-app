16caee2e5b5f087896b4f508c214d9c62e772e85
Jakub Świątkowski <sjakub7741@gmail.com>
2025-03-28 10:24:53 +0100

    updateCalendar

--- src/main/resources/templates/script.js
+++ src/main/resources/templates/script.js
@@ -38,7 +38,7 @@ const monthName = ["January", "February", "March", "April", "May", "June", "July
     "August", "October", "November", "December"];
 
 const renderCalendar = () => {
-    let firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+    let firstDayOfMonth = new Date(currentYear, currentMonth, 0).getDay();
     lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
     lastDayOfMonth = new Date(currentYear, currentMonth, lastDateOfMonth).getDay();
     lastDateOfLastMonth = new Date(currentYear, currentMonth, 0).getDate();
@@ -49,10 +49,11 @@ const renderCalendar = () => {
     }
 
     for (let index = 1; index <= lastDateOfMonth; index++) {
-        liTag += `<li>${index}</li>`;
+        let isToday = index === date.getDate() && currentMonth === date.getMonth() && currentYear === date.getFullYear() ? "active" : "";
+        liTag += `<li class="${isToday}">${index}</li>`;
     }
 
-    for (let i = lastDayOfMonth; i < 6; i++) {
+    for (let i = lastDayOfMonth; i <= 6; i++) {
         liTag += `<li class="inactive">${i - lastDayOfMonth + 1}</li>`
     }
 
@@ -64,6 +65,14 @@ renderCalendar();
 prevNextIcon.forEach(icon => {
     icon.addEventListener("click", () => {
         currentMonth = icon.id === "prev" ?  currentMonth - 1 : currentMonth + 1;
+
+        if(currentMonth < 0 || currentMonth > 11) {
+            date = new Date(currentYear, currentMonth);
+            currentYear = date.getFullYear();
+            currentMonth = date.getMonth();
+        } else {
+            date = new Date();
+        }
         renderCalendar();
     }) 
 });
