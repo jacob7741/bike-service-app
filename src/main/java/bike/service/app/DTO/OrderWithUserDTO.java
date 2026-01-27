6473b488899f35303c597978745e2e683a4aa064
Jakub Świątkowski <sjakub7741@gmail.com>
2026-01-27 22:44:44 +0100

    dtodto

--- src/main/java/bike/service/app/DTO/OrderWithUserDTO.java
+++ src/main/java/bike/service/app/DTO/OrderWithUserDTO.java
@@ -4,29 +4,18 @@ import bike.service.app.model.Bike;
 import bike.service.app.model.Order;
 import bike.service.app.model.Order.Status;
 import bike.service.app.model.Users;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class OrderWithUserDTO {
 
     private long orderId;
     private long userId;
-    private int clientId;
+    private String client;
     private long bikeId;
     private String firstName;
     private String lastName;
     private Status status;
-
-    private String client;
-    private Bike bike;
     
 
     public OrderWithUserDTO(Order order, Users user) {
@@ -36,10 +25,11 @@ public class OrderWithUserDTO {
         this.firstName = user.getFirstName();
         this.lastName = user.getLastName();
         this.bikeId = order.getBike().getBikeId();
-        this.clientId = order.getClient().getClientId();
-        
-        /* skończyłem tutaj */
         this.client = order.getClient().getFirst_name();
+        
+        /* skończyłem tutaj, lecz trzeba ustawić aby przy kliencie
+        były zapisane te inne dane */
+        // this.client = order.getClient().getFirst_name();
     }
     
 }
