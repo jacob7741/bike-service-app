8b3ba5fe4d679e39feb04dc63b2c8be7812aa14e
Jakub Świątkowski <sjakub7741@gmail.com>
2026-02-26 19:35:43 +0100

    opUp

--- src/main/java/bike/service/app/DTO/CreateDTOBike.java
+++ src/main/java/bike/service/app/DTO/CreateDTOBike.java
@@ -1,19 +1,5 @@
 package bike.service.app.DTO;
 
-import bike.service.app.model.Bike;
-import bike.service.app.model.Order;
-
 public class CreateDTOBike {
-    private long bikeId;
-    private String brand;
-    private String modelType;
-    private int serialNumber;
-
-    public CreateDTOBike(Bike bike, Order order) {
-        bike.setBikeId(bikeId);
-        bike.setBrand(brand);
-        bike.setModelType(modelType);
-        bike.setSerialNumber(serialNumber);
-        bike.setOrderId(order.getOrderId());
-    }
+    
 }
